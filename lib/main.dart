d6a761d6ee5a94587121a7a1d65f1ffff5e266c5
allexis096 <allexissoares@gmail.com>
2021-05-22 11:45:10 -0300

    :zap: chore: transform into StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+main() {
+  runApp(AskProject());
 }
 
-class MyApp extends StatelessWidget {
+class AskProjectState extends State<AskProject> {
+  int selectedQuestion = 0;
+
+  void answer() {
+    setState(() {
+      selectedQuestion++;
+    });
+    print(selectedQuestion);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final List<String> questions = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Text(questions[selectedQuestion]),
+            ElevatedButton(onPressed: answer, child: Text('Resposta 1')),
+            ElevatedButton(onPressed: answer, child: Text('Resposta 2')),
+            ElevatedButton(onPressed: answer, child: Text('Resposta 3')),
+          ],
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    //
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            //
-            //
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: Icon(Icons.add),
-        ));
+class AskProject extends StatefulWidget {
+  AskProjectState createState() {
+    return AskProjectState();
   }
 }
