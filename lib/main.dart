c955102582c323379ab052573707ac5ce85c0f35
allexis096 <allexissoares@gmail.com>
2021-05-23 13:01:14 -0300

    :zap: chore: updated main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:ask_project/answer.dart';
-import 'package:ask_project/question.dart';
+import 'package:ask_project/quiz.dart';
+import 'package:ask_project/result.dart';
 import 'package:flutter/material.dart';
 
 main() {
@@ -8,35 +8,73 @@ main() {
 
 class _AskProjectState extends State<AskProject> {
   int _selectedQuestion = 0;
+  int _totalPoint = 0;
 
-  void _answer() {
+  void _answer(int point) {
+    if (hasSelectedQuestion) {
+      setState(() {
+        _selectedQuestion++;
+        _totalPoint += point;
+      });
+    }
+  }
+
+  void _restartQuiz() {
     setState(() {
-      _selectedQuestion++;
+      _selectedQuestion = 0;
+      _totalPoint = 0;
     });
-    print(_selectedQuestion);
+  }
+
+  final List<Map> _questions = const [
+    {
+      'text': 'Qual é a sua cor favorita?',
+      'answers': [
+        {'text': 'preto', 'points': 10},
+        {'text': 'vermelho', 'points': 5},
+        {'text': 'verde', 'points': 3},
+        {'text': 'branco', 'points': 1}
+      ],
+    },
+    {
+      'text': 'Qual é o seu animal favorito?',
+      'answers': [
+        {'text': 'coelho', 'points': 10},
+        {'text': 'cobra', 'points': 5},
+        {'text': 'elefante', 'points': 3},
+        {'text': 'leão', 'points': 1},
+      ],
+    },
+    {
+      'text': 'Qual é o seu instrutor favorito?',
+      'answers': [
+        {'text': 'maria', 'points': 10},
+        {'text': 'joão', 'points': 5},
+        {'text': 'leo', 'points': 3},
+        {'text': 'pedro', 'points': 1},
+      ],
+    },
+  ];
+
+  bool get hasSelectedQuestion {
+    return _selectedQuestion < _questions.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<String> questions = [
-      'Qual é a sua cor favorita?',
-      'Qual é o seu animal favorito?',
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
           centerTitle: true,
         ),
-        body: Column(
-          children: [
-            Question(questions[_selectedQuestion]),
-            Answer('Resposta 1', _answer),
-            Answer('Resposta 2', _answer),
-            Answer('Resposta 3', _answer),
-          ],
-        ),
+        body: hasSelectedQuestion
+            ? Quiz(
+                questions: _questions,
+                selectedQuestion: _selectedQuestion,
+                answer: _answer,
+              )
+            : Result(_totalPoint, _restartQuiz),
       ),
     );
   }
